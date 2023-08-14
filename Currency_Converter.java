9904322ba0f602465bf92989d65d954fa9991f88
vinisha2709 <115263829+vinisha2709@users.noreply.github.com>
2023-08-14 12:01:02 +0530

    Create Currency_Converter.java

---
+++ Currency_Converter.java
@@ -0,0 +1,50 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+package com.techvidvan.Currency_Coverter;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ItemEvent;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class CurrencyConverter extends JFrame {
+   private JComboBox choiceFrom;
+   private JComboBox choiceTo;
+   private JTextField input;
+   private JButton jButton1;
+   private JLabel jLabel1;
+   private JLabel jLabel2;
+   private JLabel jLabel3;
+   private JLabel output;
+   private JLabel title;
+
+   public CurrencyConverter() {
+      throw new Error("Unresolved compilation problem: \n\tThe declared package \"com.convert.CurrencyCoverter\" does not match the expected package \"com.techvidvan.CurrencyCoverter\"\n");
+   }
+
+   private void initComponents() {
+      throw new Error("Unresolved compilation problem: \n");
+   }
+
+   private void jButton1ActionPerformed(ActionEvent var1) {
+      throw new Error("Unresolved compilation problem: \n");
+   }
+
+   private void inputActionPerformed(ActionEvent var1) {
+      throw new Error("Unresolved compilation problem: \n");
+   }
+
+   private void choiceFromItemStateChanged(ItemEvent var1) {
+      throw new Error("Unresolved compilation problem: \n");
+   }
+
+   private void choiceToItemStateChanged(ItemEvent var1) {
+      throw new Error("Unresolved compilation problem: \n");
+   }
+
+   public static void main(String[] var0) {
+      throw new Error("Unresolved compilation problem: \n");
+   }
+}
